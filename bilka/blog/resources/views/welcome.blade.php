a4365283ea2dc03367c895bf0aa3b7956db72f3d
77dasha0377 <77dasha0377@gmail.com>
2020-05-28 17:34:10 +0300

    some commit

--- bilka/blog/resources/views/welcome.blade.php
+++ bilka/blog/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ALOHA
                 </div>
 
                 <div class="links">
--- config.php
+++ config.php
@@ -2,4 +2,4 @@
 define('DB_NAME','dblopatkin');
 define('DB_USER','lopatkin');
 define('DB_PASSWORD','123456');
-define('DB_HOST','localhost');
+define('DB_HOST','192.168.1.17');
